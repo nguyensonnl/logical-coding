318ae95520c65e05b7d67bcaf33811c2c62f81ef
nguyensonnl <nguyenquynhson30446@gmail.com>
2024-11-22 19:06:07 +0700

    update

--- 1. mindset_coding/loops/2. tamGiac01.js
+++ 1. mindset_coding/loops/2. tamGiac01.js
@@ -19,5 +19,18 @@ function tamGiac01() {
   }
   console.log(str);
 }
-
 tamGiac01();
+
+function text() {
+  {
+    let str = "";
+    for (let i = 1; i <= 5; i++) {
+      for (let j = 1; j <= i; j++) {
+        str += "*";
+      }
+      str += "\n";
+    }
+    console.log(str);
+  }
+}
+text();
